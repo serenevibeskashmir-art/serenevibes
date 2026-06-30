2debf57e197b1d1f3d1016505b6a13bbc90804f1
serenevibeskashmir-art <serenevibeskashmir@gmail.com>
2026-06-30 22:00:12 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./frontendsrcApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .catch((err) => console.error("Service worker registration failed:", err));
+  });
+}
